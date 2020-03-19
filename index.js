26f8203b6472a781b0cad01eec106df3c9706ae0
Agandaur <cjwagstaffe@gmail.com>
2020-03-18 17:09:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 12 / 3
+
+let max = Math.max(1, 2, 3, 4, 5, 20)

dac74be11a7fafda5b31625efb82b61390dcc2b6
kailanak1 <kailanakahawaii@gmail.com>
2020-03-19 11:21:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 2 
+let num2 = 31 
+
+let multiply = num1 * num2 
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let x = 24
+let y = 5
+let mod = x % y
+
+let set = (1,2,20)
+
+let max = Math.max(set)
+
+
+

0c769f18505f999338b8fe9df9ff8a61734168c8
github username <jonathanriecke@gmail.com>
2020-03-19 16:32:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+
+
+let num2 = 31
+let num1 = 2
+let multiply = (num1 * num2)
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = (24 % 5)
+let max = Math.max(1,5,16,20)
